796e6bae814a90be529d22dfb0f9be103fe8fcbc
Bill Thiede <git@xinu.tv>
2012-11-04 21:32:35 -0800

    Revert "Use float32 for inner loops for faster math."
    This reverts commit b748f1a27c278e64c391209ce1cbe5ec70f5681d.

--- fractal/mandelbrot/mandelbrot.go
+++ fractal/mandelbrot/mandelbrot.go
@@ -69,35 +69,24 @@ func (m *Mandelbrot) ColorIndexAt(x, y int) uint8 {
 	return m.ComputeMembership(r, i)
 }
 
-func mult(x, y, u, v float32) (r, i float32) {
-	return (x*u - y*v), (x*v + y*u)
-}
-
-func absLessThan(r, i, v float32) bool {
-	return (r*r + i*i) < (v * v)
-}
-
 // Takes in a coordinate in fractal space, and returns an index to the proper
 // coloring for that point
 func (m *Mandelbrot) ComputeMembership(r, i float64) uint8 {
-	zr, zi := float32(r), float32(i)
-	wr, wi := float32(0), float32(0)
-
+	z := complex(r, i)
+	w := complex(0, 0)
 	// Start at -1 so the first escaped values get the first color.
 	it := -1
-	for absLessThan(wr, wi, 2) && (it < m.maxIterations) {
-		vr := wr
-		vi := wi
+	for fractal.AbsLessThan(w, 2) && (it < m.maxIterations) {
+		v := w
 		for i := 1; i < m.order; i++ {
-			vr, vi = mult(vr, vi, wr, wi)
+			v *= w
 		}
-		wr = vr + zr
-		wi = vi + zi
+		w = v + z
 
 		it++
 	}
 
-	if absLessThan(wr, wi, 2) {
+	if fractal.AbsLessThan(w, 2) {
 		// Pixel in mandelbrot set, return black
 		return 0
 	}
